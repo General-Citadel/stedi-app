668986a15744a87087b783823bad5f011abc9ca8
Stanley Black <stanleycit@gmail.com>
2023-01-19 10:43:49 -0700

    Login api good and bad added to validate api integration

---
+++ tests/loginapi.test.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+it ("Should get a login token", async () => {//async allows us to read from the database by moving on and coming back
+
+    const loginResponse = await fetch('https://dev.stedi.me/login', { //await fetch allows it to move on while getting the data
+
+    method: 'POST', //method: POST says to give informaiton to the server
+    headers:{
+        'Content-Type': 'application/text'
+    },
+
+    body:JSON.stringify({//makes the following into JSON text to be read by the server
+        "userName": "stanleycit@gmail.com",
+        "password": "C@0lClass"
+    })
+})
+expect(loginResponse.status).toBe(200); //200 means authorized 
+})
